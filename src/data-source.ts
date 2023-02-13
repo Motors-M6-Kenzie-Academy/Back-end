22c2b2c0f957ee3cd5adead25e5ac5a12164f3b8
larissa-sato <larissasato_@outlook.com>
2023-02-13 12:24:46 -0300

    started src folder, added libs

---
+++ src/data-source.ts
@@ -0,0 +1,48 @@
+import { DataSource, DataSourceOptions } from "typeorm";
+import path from "path";
+import "dotenv/config";
+import "reflect-metadata";
+
+const setDataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath: string = path.join(__dirname, "./entities/**.{js,ts}");
+  const migrationsPath: string = path.join(
+    __dirname,
+    "./migrations/**.{js,ts}"
+  );
+
+  const nodeEnv = process.env.NODE_ENV;
+
+  if (nodeEnv === "production") {
+    return {
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    };
+  }
+
+  if (nodeEnv === "test") {
+    return {
+      type: "sqlite",
+      database: ":memory:",
+      synchronize: true,
+      entities: [entitiesPath],
+    };
+  }
+
+  return {
+    type: "postgres",
+    host: process.env.HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    port: 5432,
+    database: process.env.POSTGRES_DB,
+    synchronize: false,
+    logging: true,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+  };
+};
+
+const dataSourceConfig = setDataSourceConfig();
+export default new DataSource(dataSourceConfig);
